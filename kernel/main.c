d4d7c6fef475fbeb594d92e3116c83dceef2715e
jack520404@gmail.com <igusa@Thinkpad.localdomain>
2025-10-09 23:52:59 +0800

    add put_int

--- kernel/main.c
+++ kernel/main.c
@@ -2,5 +2,12 @@
 void main(void) {
    
     put_str("I am kernel\n");
+    put_int(0);
+    put_char('\n');
+    put_int(1234567890);
+    put_char('\n');
+    put_int(0xabcdef);
+    put_char('\n');
+    put_int(0x9);
     while(1);
 }   
