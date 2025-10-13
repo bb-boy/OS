296a6b523f3b8827d5c8c4569b054ac375844d26
jack520404@gmail.com <igusa@Thinkpad.localdomain>
2025-10-14 02:42:28 +0800

    once

--- kernel/main.c
+++ kernel/main.c
@@ -5,6 +5,7 @@ void main(void) {
    
     put_str("I am kernel\n");
     init_all();
+    put_str("Initialization complete, enabling interrupts...\n");
     asm volatile ("sti");//临时开中断
     while(1);
 }   
